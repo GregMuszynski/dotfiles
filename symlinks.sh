aa162cae8a01dd1dff62dd93d66412135d892361
Greg Muszynski <>
2025-05-18 22:46:31 +0000

    Essentials - a sensible starting point

---
+++ symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -s ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -s ~/dotfiles/.config/alacritty/themes/nordfox.toml ~/.config/alacritty/themes/nordfox.toml
+ln -s ~/dotfiles/.config/sway/config ~/.config/sway/config
+ln -s ~/dotfiles/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
+ln -s ~/dotfiles/.config/waybar/style.css ~/.config/waybar/style.css
+ln -s ~/dotfiles/.config/wofi/config ~/.config/wofi/config
+ln -s ~/dotfiles/.config/wofi/style.css ~/.config/wofi/style.css
