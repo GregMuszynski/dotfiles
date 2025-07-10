a6aa9e7b6bffc2b40ecf838265cc20db5fc62feb
Greg Muszynski <>
2025-07-10 23:11:13 +0100

    Updated symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -2,8 +2,10 @@
 ln -s ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -s ~/dotfiles/.config/alacritty/themes/nordfox.toml ~/.config/alacritty/themes/nordfox.toml
 ln -s ~/dotfiles/.config/alacritty/themes/catppuccin_frappe.toml ~/.config/alacritty/themes/catppuccin_frappe.toml
-ln -s ~/dotfiles/.config/sway/config ~/.config/sway/config
-ln -s ~/dotfiles/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
-ln -s ~/dotfiles/.config/waybar/style.css ~/.config/waybar/style.css
-ln -s ~/dotfiles/.config/wofi/config ~/.config/wofi/config
-ln -s ~/dotfiles/.config/wofi/style.css ~/.config/wofi/style.css
+# ln -s ~/dotfiles/.config/sway/config ~/.config/sway/config
+# ln -s ~/dotfiles/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
+# ln -s ~/dotfiles/.config/waybar/style.css ~/.config/waybar/style.css
+# ln -s ~/dotfiles/.config/wofi/config ~/.config/wofi/config
+# ln -s ~/dotfiles/.config/wofi/style.css ~/.config/wofi/style.css
+ln -s ~/dotfiles/.config/i3/config ~/.config/i3/config
+ln -s ~/dotfiles/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
